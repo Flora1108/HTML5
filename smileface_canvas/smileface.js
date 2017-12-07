d8e737cbf2805683d524143866e882cb6526a2c2
guoyuanfei107 <guoyuanfei107@pingan.com.cn>
2017-12-07 14:09:43 +0800

    用画布绘制笑脸

---
+++ smileface_canvas/smileface.js
@@ -0,0 +1,33 @@
+window.onload = function(){
+	drawSmileFace();
+};
+function drawSmileFace(){
+	var canvas = document.getElementById("smilecanvas");
+	var context = canvas.getContext("2d");
+	
+	context.beginPath();
+	context.arc(300,300,200,0,degreesToRadians(360),true);
+	context.fillStyle = "#ffffcc";
+	context.fill();
+	context.stroke();
+	
+	context.beginPath();
+	context.arc(200,250,25,0,degreesToRadians(360),true);
+	context.stroke();
+	
+	context.beginPath();
+	context.arc(400,250,25,0,degreesToRadians(360),true);
+	context.stroke();
+	
+	context.beginPath();
+	context.moveTo(300,300);
+	context.lineTo(300,350);
+	context.stroke();
+	
+	context.beginPath();
+	context.arc(300,350,75,degreesToRadians(20),degreesToRadians(160),false);
+	context.stroke();
+}
+function degreesToRadians(degrees){
+		return (degrees * Math.PI)/180;
+}
